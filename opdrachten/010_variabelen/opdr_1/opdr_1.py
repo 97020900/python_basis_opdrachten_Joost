36371933c8948b87157e4280c3d87d631f356d3b
97020900 <97020900@st.deltion.nl>
2024-09-26 14:16:28 +0200

    Print gefikst

--- opdrachten/010_variabelen/opdr_1/opdr_1.py
+++ opdrachten/010_variabelen/opdr_1/opdr_1.py
@@ -1,10 +1,11 @@
 # Opdracht 1
-# Naam student:
+# Naam student: Joost Dommerholt
 # Groep:
 
 # Hier komt je code...
 
-my_list = ...
+# Waarden in een lijst stoppen en afdrukken
+my_list = [13, "wat een weertje", 12.5, 8]
 
-a = 5
-print (a)
+# Printen van de lijst
+my_list
