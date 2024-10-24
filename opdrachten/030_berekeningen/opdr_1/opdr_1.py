516b7e65d0e497e14b7bf69cec130689fa477208
97020900 <97020900@st.deltion.nl>
2024-10-24 12:02:03 +0200

    030 afgerond

--- opdrachten/030_berekeningen/opdr_1/opdr_1.py
+++ opdrachten/030_berekeningen/opdr_1/opdr_1.py
@@ -1,5 +1,20 @@
 # Opdracht 1 berekeningen
-# Naam student:
+# Naam student: Joost Dommerholt
 # Groep:
 
 # Hier komt je code...
+
+Bas = 29
+Wesley = 30
+Jeffery = 28
+
+# Totale leeftijd berekenen
+totale_leeftijd = Bas + Wesley + Jeffery
+
+# Gemiddelde leeftijd berekenen
+gemiddelde_leeftijd = totale_leeftijd / 3
+
+# Resultaten printen
+print(f"Dit weten we over Bas, Wesley en Jeffery:")
+print(f"De totale waarde van alle leeftijden bij elkaar opgeteld = {totale_leeftijd}")
+print(f"De gemiddelde leeftijd van deze boys is: {gemiddelde_leeftijd:.0f}")
--- opdrachten/030_berekeningen/opdr_2/opdr_2.py
+++ opdrachten/030_berekeningen/opdr_2/opdr_2.py
@@ -1,10 +1,33 @@
 # Opdracht 2 berekeningen
-# Naam student:
+# Naam student: Joost Dommerholt
 # Groep:
 
 # Hier komt je code...
 
-c = ...
-f = ...
+# Waarden
+c = -12
+f = 102
 
-print()
+# Van Celsius naar Fahrenheit
+fahrenheit = (9/5) * c + 32
+
+# Van Fahrenheit naar Celsius
+celsius = (5/9) * (f - 32)
+
+# Resultaten printen
+print(f"{c} graden Celsius is gelijk aan {fahrenheit:.1f} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {celsius:.1f} graden Celsius")
+
+# Nieuwe waarden
+c = 62.2
+f = 96
+
+# Van Celsius naar Fahrenheit
+fahrenheit = (9/5) * c + 32
+
+# Van Fahrenheit naar Celsius
+celsius = (5/9) * (f - 32)
+
+# Resultaten printen
+print(f"{c} graden Celsius is gelijk aan {fahrenheit:.0f} graden Fahrenheit")
+print(f"{f} graden Fahrenheit is gelijk aan {celsius:.1f} graden Celsius")
